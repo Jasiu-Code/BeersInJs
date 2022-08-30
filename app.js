31b6319de2ae97fa47ff9ffd3cd9db94574a52e8
Jasiu MacbookAir <jan@kaletka.com.pl>
2022-08-30 20:48:57 +0200

    added selecting favourite

---
+++ app.js
@@ -0,0 +1,35 @@
+///for grab all divs add evenet listener // wywyowalnie tej funcki po fetchu
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  const beers = document.getElementById("beers");
+  function addFav() {
+    this.classList.contains("fav")
+      ? this.classList.remove("fav")
+      : this.classList.add("fav");
+  }
+  fetch("https://api.punkapi.com/v2/beers")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data[0]);
+      data.map((beer) => {
+        const container = document.createElement("div");
+        container.classList.add("beerContainer");
+        container.addEventListener("click", addFav);
+        const title = document.createElement("h3");
+        const image = document.createElement("img");
+        const description = document.createElement("p");
+        title.innerText = beer.name;
+        image.src = beer.image_url;
+        description.innerText = beer.description;
+        container.appendChild(title);
+        container.appendChild(image);
+        container.appendChild(description);
+        beers.append(container);
+      });
+    });
+}
