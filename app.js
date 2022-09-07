411aeaa9f47a2fab9f2e357b96b44a0c3ee84c62
Jasiu MacbookAir <jan@kaletka.com.pl>
2022-09-07 08:20:03 +0200

    Add to favourite added

--- app.js
+++ app.js
@@ -1,43 +1,76 @@
-if (document.readyState == "loading") {
-  document.addEventListener("DOMContentLoaded", ready);
-} else {
-  ready();
+// if (document.readyState == "loading") {
+//   document.addEventListener("DOMContentLoaded", ready);
+// } else {
+//   ready();
+// }
+
+async function getAllBeers() {
+  const response = await fetch("https://api.punkapi.com/v2/beers");
+  const data = await response.json();
+  displayAllBeers(data);
+}
+getAllBeers();
+async function getRandomBeers() {
+  const response = await fetch("https://api.punkapi.com/v2/beers/random");
+  const data = await response.json();
+  displayRandomBeers(data);
+}
+getRandomBeers();
+
+function displayRandomBeers(data) {
+  const randomBeer = document.getElementById("randomBeer");
+  data.map((beer) => {
+    console.log(beer.name);
+    randomBeer.innerHTML = `
+        <div class='beerContainer'>
+            <h3 class='name'>${beer.name}</h3>
+            <img src='${beer.image_url}'></img>
+            <p>${beer.description}</p>
+        </div>
+        `;
+  });
 }
 
-function ready() {
-  const beers = document.getElementById("beers");
-  const favBeer = document.getElementsByClassName("fav");
-  function addFav() {
-    this.classList.contains("fav")
-      ? this.classList.remove("fav")
-      : this.classList.add("fav");
-    displayFav();
-  }
+function displayAllBeers(data) {
+  const beersContainer = document.getElementById("beersContainer");
+  data.map((beer) => {
+    console.log(beer.name);
+    beersContainer.innerHTML += `
+      <div class='beerContainer'>
+          <h3 class='name'>${beer.name}</h3>
+          <img src='${beer.image_url}'></img>
+          <p>${beer.description}</p>
+          <button onclick="addToFav('${beer.name}')">Add to Favourite</button>
+      </div>
+      `;
+  });
+}
 
-  function displayFav() {
-    favBeer.forEach(() => {
-      console.log("test");
-    });
+function addToFav(name) {
+  const favBeers = document.getElementById("favBeer");
+  const fav = favBeers.getElementsByClassName("fav");
+  console.log(fav.length);
+  for (let i = 0; i < fav.length; i++) {
+    if (fav[i].innerText == name) {
+      alert("Its already your favourite!");
+      return;
+    }
   }
+  favBeers.innerHTML += `
+  <div>
+    <li class="fav">${name}</li>
+    <button onclick="removeFav('${name}')">Remove</button>
+  </div>
+    `;
+}
 
-  fetch("https://api.punkapi.com/v2/beers")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data[0]);
-      data.map((beer) => {
-        const container = document.createElement("div");
-        container.classList.add("beerContainer");
-        container.addEventListener("click", addFav);
-        const title = document.createElement("h3");
-        const image = document.createElement("img");
-        const description = document.createElement("p");
-        title.innerText = beer.name;
-        image.src = beer.image_url;
-        description.innerText = beer.description;
-        container.appendChild(title);
-        container.appendChild(image);
-        container.appendChild(description);
-        beers.append(container);
-      });
-    });
+function removeFav(name) {
+  const fav = document.getElementsByClassName("fav");
+  for (let i = 0; i < fav.length; i++) {
+    if (fav[i].innerText == name) {
+      fav[i].parentElement.remove();
+    }
+  }
+  //   var buttonClicked = e.target;
+  //   buttonClicked.parentElement.remove();
 }
