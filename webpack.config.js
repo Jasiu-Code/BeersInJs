78ffe91f203ec8db10a6f57f9c3db79add184539
Jasiu Imac <jan@kaletka.com.pl>
2022-09-27 13:38:19 +0200

    Ready to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          // loader: 'ts-loader',
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
